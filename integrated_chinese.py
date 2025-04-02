ce1d3d58d83f63034a9e3329f7a8913e6f5b40e7
unknown <jameskowalik@outlook.com>
2025-04-02 13:15:48 +0900

    revised date

--- integrated_chinese.py
+++ integrated_chinese.py
@@ -1,6 +1,6 @@
 # Author: James Kowalik
 # Created: 25/03/25
-# Revised: 
+# Revised: 25/04/02
 # Description: Create prompt cards for practising building sentences in Chinese
 #              corresponds to content of "Integrated Chinese Level 1 Part 1"
 
