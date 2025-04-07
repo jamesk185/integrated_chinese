d255e34104fc19d5ab97027083e7fd9ecbc2cabe
unknown <jameskowalik@outlook.com>
2025-04-07 13:26:10 +0900

    help screen finalised for lesson 2 q1

--- lessons/02/l02_q01.py
+++ lessons/02/l02_q01.py
@@ -40,13 +40,13 @@ running = True
 count = 0
 img_id, demon, gender = choose_random()
 current_image = load_image(img_id)
-font = pygame.font.Font(None, 80)
+font = pygame.font.SysFont("Noto Sans", 80)
 helpscreen = None
 while running:
 	screen.fill((0, 0, 0))
 	
 	if count > 0:
-		font = pygame.font.Font(None, 120)
+		font = pygame.font.SysFont("Noto Sans", 120)
 	
 	if not helpscreen and count == 0:
 		intro_lines = [
@@ -69,9 +69,36 @@ while running:
 		screen.blit(text_surface, text_rect)
 	
 	elif helpscreen:
-		text_surface = font.render("{some help screen}", True, (255, 255, 255))
-		text_rect = text_surface.get_rect(center=(WIDTH // 2, HEIGHT // 2))
-		screen.blit(text_surface, text_rect)
+		font = pygame.font.SysFont("Noto Sans", 20)
+		
+		help_lines = [
+			"Who is .. ? = ... shì shéi?",
+			"that ... = nà ...",
+			"this ... = zhè ...",
+			"person = rén",
+			"man = nán rén",
+			"woman = nǚ rén",
+			"boy = nán háizi",
+			"girl = nǚ háizi",
+			"*** Don't forget 'ge' after this/that",
+			"",
+			"M = man = lǐ kāng shēng",
+			"F = woman = sūn lì",
+			"m = boy = xià yu",
+			"f = girl = liú chǔ tián",
+			"",
+			"Who is that man = nà ge nán rén shì shéi",
+			"Who is this woman = zhè ge nǚ rén shì shéi",
+			"Who is this boy = zhè ge nán háizi shì shéi",
+			"Who is that girl = nà ge nǚ háizi shì shéi"
+		]
+		line_height = 27.5  # Adjust spacing as needed
+		start_y = 50
+	
+		for i, line in enumerate(help_lines):
+			text_surface = font.render(line, True, (200, 200, 200))
+			text_rect = text_surface.get_rect(center=(WIDTH // 2, start_y + i * line_height))
+			screen.blit(text_surface, text_rect)
 	
 	else:
 		img_rect = current_image.get_rect(center=(WIDTH // 2, HEIGHT // 2))
